f283dc3aefb090a930f2c4ca1ec63baa6d71f5f8
Neil Scheuermann <neilscheuermann@gmail.com>
2020-05-03 15:54:23 -0600

    More formatting on nav-bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,14 @@ import styled from "styled-components"
 import { navLinks, MOBILE_WIDTH } from "../utils/constants"
 
 function NavBar() {
-  const renderMenuLinks = navLinks.map(navLink => {
+  const renderNavLinks = navLinks.map(navLink => {
     const { name, linkTo } = navLink
+    const isCurrentPage = () => {
+      // see if current page matches the name
+    }
 
     return (
-      <NavLink key={name}>
+      <NavLink key={name} active={isCurrentPage()}>
         <Link
           style={{
             boxShadow: `none`,
@@ -17,41 +20,45 @@ function NavBar() {
           }}
           to={linkTo}
         >
-          {name}
+          {name.toUpperCase()}
         </Link>
       </NavLink>
     )
   })
 
-  return (
-    <Container>
-      <LinksContainer>{renderMenuLinks}</LinksContainer>
-    </Container>
-  )
+  return <LinksListWrapper>{renderNavLinks}</LinksListWrapper>
 }
 
-const Container = styled.nav`
+const LinksListWrapper = styled.nav`
   position: fixed;
   width: 100vw;
   z-index: 1;
 
-  @media only screen and (max-width: ${MOBILE_WIDTH}) {
-    bottom: 0;
-  }
+  display: flex;
 
   @media only screen and (min-width: ${MOBILE_WIDTH}) {
     top: 0;
+    justify-content: flex-end;
   }
-`
 
-const LinksContainer = styled.div`
-  display: flex;
+  @media only screen and (max-width: ${MOBILE_WIDTH}) {
+    bottom: 0;
+    justify-content: space-between;
+  }
 `
 
 const NavLink = styled.nav`
   align-items: center;
   display: flex;
   height: 100%;
+  border: solid red 1px;
+  padding: 0 20px;
+
+  ${({ active }) =>
+    active &&
+    `
+      border-top: 4px solid black;
+    `}
 `
 
 export default NavBar
