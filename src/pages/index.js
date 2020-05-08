a985dc79e29de9b51e7c805f561fe75e2f010055
Neil Scheuermann <neilscheuermann@gmail.com>
2020-05-08 09:40:42 -0600

    Remove un-used variables

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
