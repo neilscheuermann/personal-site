78cd84793e8d613784dd44ff6d2a16e8c2f1a230
Neil Scheuermann <neilscheuermann@gmail.com>
2021-01-07 16:52:28 -0700

    Adjusts size of home page image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Img from 'gatsby-image'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
@@ -9,26 +10,44 @@ export default function HomePage({ data, location }) {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="Home" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
-      <h1>
-        Hey people{' '}
-        <span role="img" aria-label="laptop emoji">
-          💻
-        </span>
-      </h1>
-      <p>
-        I'm a software engineer who specializes in JavaScript/React frontend,
-        and Elixir/Ecto on the backend.
-      </p>
-      {/* TODO>>>>: Maybe try something like this for my home page. */}
-      {/* (http://ianenders.com/) */}
-      {/*  */}
-      {/* TODO>>>: I kinda like the simple layout of this site. Something along */}
-      {/* these lines. (https://seanoconn.org/) */}
-      <Img fluid={data.headshot.childImageSharp.fluid} />
+      <HomePageStyles>
+        <SEO
+          title="Home"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        <div class="cover-image">
+          <Img
+            fluid={data.headshot.childImageSharp.fluid}
+            imgStyle={{
+              borderRadius: `50%`,
+            }}
+          />
+        </div>
+        <h1>
+          Hey people{' '}
+          <span role="img" aria-label="laptop emoji">
+            💻
+          </span>
+        </h1>
+        <p>
+          I'm a software engineer who specializes in JavaScript/React frontend,
+          and Elixir/Ecto on the backend.
+        </p>
+        {/* TODO>>>>: Maybe try something like this for my home page. */}
+        {/* (http://ianenders.com/) */}
+        {/*  */}
+        {/* TODO>>>: I kinda like the simple layout of this site. Something along */}
+        {/* these lines. (https://seanoconn.org/) */}
+      </HomePageStyles>
     </Layout>
   )
 }
+
+const HomePageStyles = styled.div`
+  .cover-image {
+    width: 10vw;
+  }
+`
 
 export const query = graphql`
   query {
