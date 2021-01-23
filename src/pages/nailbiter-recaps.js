133edac9b2512bd544b1e3bf34091214ef8e40d2
Neil Scheuermann <neilscheuermann@gmail.com>
2021-01-22 23:46:23 -0700

    Adds placeholder video in tile & search on YouTube button

--- src/pages/nailbiter-recaps.js
+++ src/pages/nailbiter-recaps.js
@@ -10,29 +10,39 @@ export default function NailbiterRecapsPage({ data, location }) {
       <SEO title="Nailbiter Recaps" />
       <PageStyles>
         <p>This is the nailbiters page</p>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/Yfcyk8T7bY0"
-          title="this will be the ${videoHighlight.title}"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
         {/* TODO>>>: Find a way to search in youtube `nba full game highlights */}
         {/* tricode tricode date` */}
         {/* Then select the first video in the result. */}
         {/* Then block the progress bar and time. */}
-        {data.games.nodes.map(game => (
-          <div>
-            <h3>{game.gameUrlCode}</h3>
-            {game.period.current > 4 && 'Went into overtime!!!'}
-            <p>
-              {game.vTeam.score} - {game.hTeam.score}
-            </p>
-            <p>{game.startDateEastern}</p>
-          </div>
-        ))}
+        {data.games.nodes.map(game => {
+          const [month, day, year] = game.startDateEastern.split(' ')
+          const { vTeam, hTeam, highlightVideoUrl, startDateEastern } = game
+
+          return (
+            <div>
+              <h3>
+                {vTeam.nickname} at {hTeam.nickname}
+              </h3>
+              <p>{startDateEastern}</p>
+              <a
+                href={`https://www.youtube.com/results?search_query=${vTeam.nickname}+at+${hTeam.nickname}+${month}+${day}+${year}`}
+              >
+                Search on YouTube
+              </a>
+              <div className="video-container">
+                <iframe
+                  width="560"
+                  height="315"
+                  src={highlightVideoUrl}
+                  title={`NBA higlight of ${vTeam.nickname} at ${hTeam.nickname}, ${month}/${day}/${year}`}
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                />
+              </div>
+            </div>
+          )
+        })}
       </PageStyles>
     </Layout>
   )
@@ -42,15 +52,19 @@ const PageStyles = styled.div``
 
 export const query = graphql`
   query {
-    games: allNailbiterItem {
+    games: allNailbiterItem(
+      sort: { fields: startDateEastern, order: DESC }
+      limit: 10
+    ) {
       nodes {
         gameUrlCode
+        highlightVideoUrl
+        startDateEastern(formatString: "MMM DD YYYY")
         hTeam {
-          score
+          nickname
         }
-        startDateEastern(formatString: "MMMM DD, YYYY")
         vTeam {
-          score
+          nickname
         }
         period {
           current
