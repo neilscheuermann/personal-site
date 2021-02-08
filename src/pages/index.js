6558accb14a55faa4115ed375064f1686603ee02
Neil Scheuermann <neilscheuermann@gmail.com>
2021-02-08 07:19:56 -0700

    Slight update to index page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { MOBILE_MAX_WIDTH } from '../styles/GlobalStyles'
 
 export default function HomePage({ data, location }) {
   const siteTitle = 'Neil Scheuermann'
@@ -15,24 +16,33 @@ export default function HomePage({ data, location }) {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <div className="cover-image">
-          <Img
-            fluid={data.headshot.childImageSharp.fluid}
-            imgStyle={{
-              borderRadius: `50%`,
-            }}
-          />
-        </div>
-        <h1>
-          Hey people{' '}
-          <span role="img" aria-label="laptop emoji">
-            💻
-          </span>
-        </h1>
-        <p>
-          I'm a software engineer who specializes in JavaScript/React frontend,
-          and Elixir/Ecto on the backend.
-        </p>
+        <SectionOneStyles>
+          <div className="cover-image">
+            <Img
+              fluid={data.headshot.childImageSharp.fluid}
+              imgStyle={{
+                borderRadius: `16px`,
+              }}
+            />
+          </div>
+          <h1>
+            I'm Neil{' '}
+            <span role="img" aria-label="laptop emoji">
+              💻
+            </span>
+          </h1>
+          <p>
+            I'm a software engineer who specializes in{' '}
+            <strong>JavaScript/React</strong> on the frontend, and{' '}
+            <strong>Elixir/Ecto</strong> on the backend.
+          </p>
+          <p>
+            My goal is to enjoy the life I've been given and leave a positive
+            impact on this world.
+          </p>
+          <h2>My Journey</h2>
+          <p>Coming soon...</p>
+        </SectionOneStyles>
         {/* TODO>>>>: Maybe try something like this for my home page. */}
         {/* (http://ianenders.com/) */}
         {/*  */}
@@ -43,9 +53,28 @@ export default function HomePage({ data, location }) {
   )
 }
 
-const HomePageStyles = styled.div`
+const HomePageStyles = styled.div``
+
+const SectionOneStyles = styled.div`
   .cover-image {
     width: 10vw;
+    float: right;
+    margin-left: 16px;
+
+    @media (max-width: ${MOBILE_MAX_WIDTH}) {
+      width: initial;
+      float: initial;
+      margin: 0 10%;
+    }
+  }
+
+  & > h1,
+  h2 {
+    text-align: center;
+  }
+
+  & > :last-child {
+    text-align: center;
   }
 `
 
